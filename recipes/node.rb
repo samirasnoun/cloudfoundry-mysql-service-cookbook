9f993b59f6cc50810409737606827e76aa420d1d
Samir ASNOUN <samir.asnoun@gmail.com>
2012-08-27 17:06:20 +0200

    setting mysql node on isolated node, by searching cloudfoundry_nats_server role node

--- recipes/node.rb
+++ recipes/node.rb
@@ -8,6 +8,26 @@ package "sqlite3"
 package "libsqlite3-ruby"
 package "libsqlite3-dev"
 
+  node.set_unless['mysql']['bind_address'] = node.cloudfoundry_mysql_service.node.bind_address
+  node.set[:cloudfoundry_mysql_service][:node][:password] = node.mysql.server_root_password 
+  node.set[:cloudfoundry_mysql_service][:node][:user] = "root"
+
+  n_nodes = search(:node, "role:cloudfoundry_nats_server")
+  n_node = n_nodes.first
+  
+  node.set[:cloudfoundry_mysql_service][:searched_data][:nats_server][:host] = n_node.ipaddress
+  node.set[:cloudfoundry_mysql_service][:searched_data][:nats_server][:user] = n_node.nats_server.user
+  node.set[:cloudfoundry_mysql_service][:searched_data][:nats_server][:password] = n_node.nats_server.password
+  node.set[:cloudfoundry_mysql_service][:searched_data][:nats_server][:port] = n_node.nats_server.port
+  
+   max=0
+   i_nodes = search(:node, "role:cloudfoundry_mysql_node") #7role:cloudfoundry_mysql_node")
+   max = i_nodes.count
+
+   node.set[:cloudfoundry_mysql_service][:node][:index] = max
+
+
+
 cloudfoundry_component "mysql_node" do
   install_path File.join(node.cloudfoundry_common.vcap.install_path, "services", "mysql")
   bin_file     File.join(node.cloudfoundry_common.vcap.install_path, "bin", "services", "mysql_node")
